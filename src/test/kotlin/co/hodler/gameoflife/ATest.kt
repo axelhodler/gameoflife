a9987480b677123ad71b90c4453764d517c1c8c7
Axel Hodler <axel@hodler.co>
2018-06-24 10:17:41 +0200

    init evolving

--- src/test/kotlin/co/hodler/gameoflife/ATest.kt
+++ src/test/kotlin/co/hodler/gameoflife/ATest.kt
@@ -1,12 +1,24 @@
 package co.hodler.gameoflife
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class ATest {
-
     @Test
-    fun `runs`() {
-        Assertions.assertThat(true).isEqualTo(true)
+    fun `dead cell is reborn if it has three neighbors`() {
+        val cell = Cell(Status.DEAD)
+        val evolvedCell = cell.evolveWithNeighborCount(3)
+        assertThat(evolvedCell.status).isEqualTo(Status.ALIVE)
+    }
+}
+
+enum class Status {
+    DEAD,
+    ALIVE
+}
+
+class Cell(val status: Status) {
+    fun evolveWithNeighborCount(neighborCount: Int): Cell {
+        return Cell(Status.ALIVE)
     }
 }
