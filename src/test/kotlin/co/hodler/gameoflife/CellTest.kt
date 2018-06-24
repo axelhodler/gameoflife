fd036da9fdb3b8643ee010744899c7950995dd59
Axel Hodler <axel@hodler.co>
2018-06-24 10:01:37 +0200

    init from start.spring.io

---
+++ src/test/kotlin/co/hodler/gameoflife/CellTest.kt
@@ -0,0 +1,12 @@
+package co.hodler.gameoflife
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class Test {
+
+    @Test
+    fun `runs`() {
+        Assertions.assertThat(true).isEqualTo(true)
+    }
+}
