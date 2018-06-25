6abca6177783b4f7d0bb9120c16dd435817760b3
Axel Hodler <axel@hodler.co>
2018-06-25 20:52:16 +0200

    remove duplication

--- src/test/kotlin/co/hodler/gameoflife/ATest.kt
+++ src/test/kotlin/co/hodler/gameoflife/ATest.kt
@@ -1,7 +1,6 @@
 package co.hodler.gameoflife
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
@@ -11,29 +10,17 @@ import java.util.stream.Stream
 class ATest {
 
     private fun testDataProvider() = Stream.of(
-            CellTestData(currentStatus = Status.DEAD, neighborCount = 3, nextStatus = Status.ALIVE)
+            CellTestData(currentStatus = Status.DEAD, neighborCount = 3, nextStatus = Status.ALIVE),
+            CellTestData(currentStatus = Status.ALIVE, neighborCount = 1, nextStatus = Status.DEAD),
+            CellTestData(currentStatus = Status.ALIVE, neighborCount = 2, nextStatus = Status.ALIVE)
     )
 
     @ParameterizedTest
     @MethodSource("testDataProvider")
-    fun `dead cell is reborn if it has three neighbors`(testData: CellTestData) {
+    fun `follows transition rules`(testData: CellTestData) {
         val cell = Cell(testData.currentStatus)
         val evolvedCell = cell.evolveWithNeighborCount(testData.neighborCount)
         assertThat(evolvedCell.status).isEqualTo(testData.nextStatus)
-    }
-
-    @Test
-    fun `alive cell with one neighbor dies in next generation`() {
-        val cell = Cell(Status.ALIVE)
-        val evolvedCell = cell.evolveWithNeighborCount(1)
-        assertThat(evolvedCell.status).isEqualTo(Status.DEAD)
-    }
-
-    @Test
-    fun `alive cell with two neighbors stays alive`() {
-        val cell = Cell(Status.ALIVE)
-        val evolvedCell = cell.evolveWithNeighborCount(2)
-        assertThat(evolvedCell.status).isEqualTo(Status.ALIVE)
     }
 
     data class CellTestData(
