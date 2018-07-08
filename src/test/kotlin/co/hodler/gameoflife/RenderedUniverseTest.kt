13cf844baa9b9812f7d3ecf0b695dffd68a23998
Axel Hodler <axel@hodler.co>
2018-07-08 15:14:16 +0200

    refactor rendering tests

--- src/test/kotlin/co/hodler/gameoflife/RenderedUniverseTest.kt
+++ src/test/kotlin/co/hodler/gameoflife/RenderedUniverseTest.kt
@@ -1,53 +1,65 @@
 package co.hodler.gameoflife
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RenderedUniverseTest {
 
-    @Test
-    fun `renders the universe`() {
-        val grid = hashMapOf(
-                Location(0, 0) to Cell(Status.ALIVE),
-                Location(0, 1) to Cell(Status.DEAD),
-                Location(0, 2) to Cell(Status.ALIVE),
-                Location(1, 0) to Cell(Status.DEAD),
-                Location(1, 1) to Cell(Status.ALIVE),
-                Location(1, 2) to Cell(Status.DEAD),
-                Location(2, 0) to Cell(Status.ALIVE),
-                Location(2, 1) to Cell(Status.DEAD),
-                Location(2, 2) to Cell(Status.ALIVE)
-        )
-        val renderedUniverse = RenderedUniverse(Universe(grid)).render();
-
-        assertThat(renderedUniverse).isEqualTo("""
+    private fun renderingTestData() = Stream.of(
+            UniverseRenderingTestData(
+                    grid = hashMapOf(
+                            Location(0, 0) to Cell(Status.ALIVE),
+                            Location(0, 1) to Cell(Status.DEAD),
+                            Location(0, 2) to Cell(Status.ALIVE),
+                            Location(1, 0) to Cell(Status.DEAD),
+                            Location(1, 1) to Cell(Status.ALIVE),
+                            Location(1, 2) to Cell(Status.DEAD),
+                            Location(2, 0) to Cell(Status.ALIVE),
+                            Location(2, 1) to Cell(Status.DEAD),
+                            Location(2, 2) to Cell(Status.ALIVE)
+                    ),
+                    rendered = """
             O#O
             #O#
             O#O
-        """.trimIndent())
+                    """.trimIndent()
+            ),
+            UniverseRenderingTestData(
+                    grid = hashMapOf(
+                            Location(0, 0) to Cell(Status.DEAD),
+                            Location(0, 1) to Cell(Status.ALIVE),
+                            Location(0, 2) to Cell(Status.DEAD),
+                            Location(1, 0) to Cell(Status.ALIVE),
+                            Location(1, 1) to Cell(Status.DEAD),
+                            Location(1, 2) to Cell(Status.ALIVE),
+                            Location(2, 0) to Cell(Status.DEAD),
+                            Location(2, 1) to Cell(Status.ALIVE),
+                            Location(2, 2) to Cell(Status.DEAD)
+                    ),
+                    rendered = """
+            #O#
+            O#O
+            #O#
+                    """.trimIndent()
+            )
+    )
+
+    @ParameterizedTest
+    @MethodSource("renderingTestData")
+    fun `renders the universe`(testData: UniverseRenderingTestData) {
+        val renderedUniverse = RenderedUniverse(Universe(testData.grid)).render();
+
+        assertThat(renderedUniverse).isEqualTo(testData.rendered)
     }
 
-    @Test
-    fun `renders the universe - triangulate`() {
-        val grid = hashMapOf(
-                Location(0, 0) to Cell(Status.DEAD),
-                Location(0, 1) to Cell(Status.ALIVE),
-                Location(0, 2) to Cell(Status.DEAD),
-                Location(1, 0) to Cell(Status.ALIVE),
-                Location(1, 1) to Cell(Status.DEAD),
-                Location(1, 2) to Cell(Status.ALIVE),
-                Location(2, 0) to Cell(Status.DEAD),
-                Location(2, 1) to Cell(Status.ALIVE),
-                Location(2, 2) to Cell(Status.DEAD)
-        )
-        val renderedUniverse = RenderedUniverse(Universe(grid)).render();
-
-        assertThat(renderedUniverse).isEqualTo("""
-            #O#
-            O#O
-            #O#
-        """.trimIndent())
-    }
+    data class UniverseRenderingTestData(
+            val grid: HashMap<Location, Cell>,
+            val rendered: String
+    )
 }
 
 class RenderedUniverse(val universe: Universe) {
