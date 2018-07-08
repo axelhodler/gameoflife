463977ade1452c5ac7d8ba6aad03674e7104be3a
Axel Hodler <axel@hodler.co>
2018-07-08 15:16:06 +0200

    because dead is the default

--- src/test/kotlin/co/hodler/gameoflife/RenderedUniverseTest.kt
+++ src/test/kotlin/co/hodler/gameoflife/RenderedUniverseTest.kt
@@ -13,13 +13,9 @@ class RenderedUniverseTest {
             UniverseRenderingTestData(
                     grid = hashMapOf(
                             Location(0, 0) to Cell(Status.ALIVE),
-                            Location(0, 1) to Cell(Status.DEAD),
                             Location(0, 2) to Cell(Status.ALIVE),
-                            Location(1, 0) to Cell(Status.DEAD),
                             Location(1, 1) to Cell(Status.ALIVE),
-                            Location(1, 2) to Cell(Status.DEAD),
                             Location(2, 0) to Cell(Status.ALIVE),
-                            Location(2, 1) to Cell(Status.DEAD),
                             Location(2, 2) to Cell(Status.ALIVE)
                     ),
                     rendered = """
@@ -30,15 +26,10 @@ class RenderedUniverseTest {
             ),
             UniverseRenderingTestData(
                     grid = hashMapOf(
-                            Location(0, 0) to Cell(Status.DEAD),
                             Location(0, 1) to Cell(Status.ALIVE),
-                            Location(0, 2) to Cell(Status.DEAD),
                             Location(1, 0) to Cell(Status.ALIVE),
-                            Location(1, 1) to Cell(Status.DEAD),
                             Location(1, 2) to Cell(Status.ALIVE),
-                            Location(2, 0) to Cell(Status.DEAD),
-                            Location(2, 1) to Cell(Status.ALIVE),
-                            Location(2, 2) to Cell(Status.DEAD)
+                            Location(2, 1) to Cell(Status.ALIVE)
                     ),
                     rendered = """
             #O#
